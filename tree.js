8563a2e3b293a00ff55b187ecc4f52c56022e95b
momoladebrouill <momoladebrouill@gmail.com>
2024-06-17 18:36:55 +0200

    Arbre de style 💅️

--- tree.js
+++ tree.js
@@ -1,14 +1,14 @@
 import * as THREE from "https://cdn.skypack.dev/three@0.136.0";
 
 export default class Tree {
-  constructor(scene, x, y, z) {
-    this.obj = new THREE.CylinderGeometry(1, 1, 5, 20);
+  constructor() {
+		this.mesh = new THREE.Group()
+    this.obj = new THREE.CylinderGeometry(0.1, 0.1, 1.0, 20);
     this.material = new THREE.MeshLambertMaterial({ color: 0x33ff33 })
-    this.mesh = new THREE.Mesh(this.obj, this.material);
-    this.mesh.position.set(x, y, z);
+    let mesh = new THREE.Mesh(this.obj, this.material);
     let pointLight = new THREE.PointLight(0x00ff00, 1, 14);
-    pointLight.position.set(x, y + 5, z);
-    scene.add(pointLight);
-    scene.add(this.mesh);
+    pointLight.position.set(0,  1.0, 0);
+		this.mesh.add(pointLight)
+		this.mesh.add(mesh)
   }
 }
--- world.js
+++ world.js
@@ -1,7 +1,11 @@
 import * as THREE from "https://cdn.skypack.dev/three@0.136.0";
+import Tree from "./tree.js"
 
-function createFloor(color) {
-	const geometry = new THREE.PlaneGeometry( 5, 100 );
+const WORLDWIDTH = 5.0
+const WALLHEIGHT = 0.5
+
+function createFloor(w,color) {
+	const geometry = new THREE.PlaneGeometry(w, 100 );
 	const material = new THREE.MeshBasicMaterial( {color: color, side: THREE.DoubleSide} );
 	const plane = new THREE.Mesh( geometry, material );
 	return plane;
@@ -11,13 +15,25 @@ export default class World {
 	constructor(scene) {
 		this.group = new THREE.Group()
 
-		let bas = createFloor(0x660099);
+		let bas = createFloor(WORLDWIDTH,0x660099);
 		bas.rotateY(-Math.PI/2.0)
 		bas.rotateX(-Math.PI/4.0)
 		this.group.add(bas)
 
-		let gauche = createFloor(0xff0000);
+		let gauche = createFloor(WALLHEIGHT, 0xff0000);
+		gauche.position.y = WALLHEIGHT*0.75 -0.1
+		gauche.position.z = WORLDWIDTH/2.0 
+		gauche.rotateZ(-Math.PI/4.0)
 		this.group.add(gauche)
+		
+		let droite = gauche.clone()
+		droite.position.z = -WORLDWIDTH/2.0
+		this.group.add(droite)
+
+		let arbre = new Tree()
+		arbre.mesh.rotateZ(Math.PI/8.0)
+		this.group.add(arbre.mesh)
+
 		scene.add(this.group);
 	}
 }
